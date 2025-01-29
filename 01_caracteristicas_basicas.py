2b878fffd3b597f8f9fba21af8baf600035c712a
Ferran Parellada <ferran.parellada@stucom.com>
2025-01-29 17:11:26 +0100

    Ficheros iniciales

---
+++ 01_caracteristicas_basicas.py
@@ -0,0 +1,25 @@
+"""
+CARACTERÍSTICAS BÁSICAS DE PYTHON
+"""
+
+# VARIABLES
+# Una variable es un espacio en memoria para guardar datos
+# por tanto, es un contenedor
+
+# Para crear una variable...
+# identificador = valor
+
+# Hay reglas para llamar a los identificadores = nombre de la variable 
+# No se puede hacer:
+#   1variable (empezar por un número, después si lo puede llevar)
+#   $variable (ni empezar ni contener símbolos especiales)
+# De estos errores nos avisará VSC 
+
+# No debemos hacer (no son exactamente errores):
+#     Contener caracteres fuera del idioma inglás, como ñ, Ç, tildes, á, ö, etc
+#     Empezar por guión bajo (reservado para determinadas situaciones)
+
+# Qué debemos hacer:
+#     Nombrar a nuestras variables con palabras descriptivas
+#     Podemos usar más de una palabra separadas por un guión bajo (directiva PEP-8)
+#     Intentar que las línes de código no sean muy largas
